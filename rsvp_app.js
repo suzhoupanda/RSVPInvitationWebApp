64ec081aabeb272f559268d631b64bd5920e32e0
yongrenwang <czie.alexmak@gmail.com>
2017-02-03 21:26:14 +0800

    Finished implementing the filtering functionality

--- rsvp_app.js
+++ rsvp_app.js
@@ -15,6 +15,30 @@ div.appendChild(filterLabel);
 div.appendChild(filterCheckBox);
 mainDiv.insertBefore(div, ul);
 
+filterCheckBox.addEventListener('change',(e) => {
+	const isChecked = e.target.checked;
+	const lis = ul.children;
+
+	if(isChecked){
+		for(let i = 0; i < lis.length; i+= 1){
+			let li = lis[i];
+			if(li.className === 'responded'){
+				li.style.display = '';
+			} else {
+				li.style.display = 'none';
+			}
+		}
+	} else {
+		for(let i = 0; i < lis.length; i+= 1){
+			let li = lis[i];
+			li.style.display = '';
+
+		}
+
+	}
+
+});
+
 // const checkbox_span = document.getElementById('response-span');
 // const checkbox_input = checkbox_span.querySelector('input');
 
