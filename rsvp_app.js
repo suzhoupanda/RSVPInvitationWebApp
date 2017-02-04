dfe03c47c35b59bd4ffe39b5639216a8446889b4
yongrenwang <czie.alexmak@gmail.com>
2017-02-04 10:08:22 +0800

    Refactored all of the Javascript code in app.js so that it is called after the DOM is loaded; added event listener to the DOM, which executes all of the code in app.js after the HTML is loaded

--- rsvp_app.js
+++ rsvp_app.js
@@ -1,19 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => { 
 
-const form = document.getElementById('registrar');
-const input = form.querySelector('input');
+	const form = document.getElementById('registrar');
+	const input = form.querySelector('input');
 
-const mainDiv = document.querySelector('.main');
-const ul = document.getElementById('invitedList');
+	const mainDiv = document.querySelector('.main');
+	const ul = document.getElementById('invitedList');
 
-const div = document.createElement('div');
-const filterLabel = document.createElement('label');
-const filterCheckBox = document.createElement('input');
+	const div = document.createElement('div');
+	const filterLabel = document.createElement('label');
+	const filterCheckBox = document.createElement('input');
 
-filterLabel.textContent = "Hide those who haven't yet responded";
-filterCheckBox.type = 'checkbox';
-div.appendChild(filterLabel);
-div.appendChild(filterCheckBox);
-mainDiv.insertBefore(div, ul);
+	filterLabel.textContent = "Hide those who haven't yet responded";
+	filterCheckBox.type = 'checkbox';
+	div.appendChild(filterLabel);
+	div.appendChild(filterCheckBox);
+	mainDiv.insertBefore(div, ul);
 
 filterCheckBox.addEventListener('change',(e) => {
 	const isChecked = e.target.checked;
@@ -133,4 +134,7 @@ ul.addEventListener('click', (e) => {
 				button.textContent = 'edit';
 			}
 	}
+});
+
+
 });
